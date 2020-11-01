6f82dc122ed18cc1137f0b73b239793872e9f227
Mojoj <nzsombor97.hu@gmail.com>
2020-11-01 15:00:29 +0100

    Creating the basic folder sturcture for the backend (in Netbeans 11.3)

---
+++ backend/spring-boot/src/main/java/com/fullstack/webshop/SpringBootWebshopApplication.java
@@ -0,0 +1,13 @@
+package com.fullstack.webshop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootWebshopApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootWebshopApplication.class, args);
+	}
+
+}
